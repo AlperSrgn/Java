df112a58a503723e34f2458f4ab09e36082966a6
Alper Sargın Ercun <118177821+AlperSrgn@users.noreply.github.com>
2023-01-28 17:42:59 +0300

    Update Main.java

--- Average/src/Main.java
+++ Average/src/Main.java
@@ -4,8 +4,6 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-
 		/*
 		 * Develop a program to average all the elements in a 10-element integer array
 		 * of random numbers. When writing the program, write it in accordance with the
@@ -16,6 +14,7 @@ public class Main {
 		 * for averaging as a function. Let it take the array as input. Let the
 		 * averaging function return the mean as a decimal number. (return)
 		 */
+		Random random = new Random();
 
 		int[] numbers = new int[10];
 
