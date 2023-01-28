d6c81747f36a617c7053a0fd35d2adcc9b413e65
Alper Sargın Ercun <118177821+AlperSrgn@users.noreply.github.com>
2023-01-28 17:45:04 +0300

    Add files via upload

---
+++ StarTriangle/src/Main.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Write the java algorithm code that makes an asterisk triangle as much as the
+		 * integer value entered by the user from the console.
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+
+		int num = scanner.nextInt();
+
+		for (int i = 1; i <= num; i++) {
+			System.out.println("");
+			for (int j = 1; j < i; j++) {
+				System.out.print("");
+				System.out.print("*");
+			}
+			System.out.print("*");
+		}
+	}
+
+}
